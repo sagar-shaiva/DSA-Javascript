55e048e82bfcb9cf1fb9cb94163afa8ae122034e
sagar-shaiva <s.sagar2468@gmail.com>
2025-07-06 20:20:02 +0530

    palindrome of numbers

--- mathematics/script.js
+++ mathematics/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // count number of digits
+/*
 const countDigits = (n)=>{
 
     let res =0;
@@ -16,4 +17,19 @@ const countDigits = (n)=>{
 
 console.log(countDigits(1234));
 
+//time complexity θ(n); for n digits
+*/
+//Palindrome Number
+const palindrome =function (num){
+  let reverse=0,temp=num;
+    while(temp!==0){
+        let lastDigit =temp%10;
+        reverse = reverse*10 + lastDigit;
+        temp = Math.floor(temp/10);
+    }
+
+    return reverse === num;
+}
+
+console.log('Is palindrome:',palindrome(121));
 //time complexity θ(n); for n digits
